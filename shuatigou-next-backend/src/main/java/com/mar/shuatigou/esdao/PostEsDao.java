f4e05703b5e74b6e5c00173d3971e5f20f3a571c
Mar1f <938224402@qq.com>
2024-11-02 17:32:09 +0800

    Initial commit

---
+++ shuatigou-next-backend/src/main/java/com/mar/shuatigou/esdao/PostEsDao.java
@@ -0,0 +1,14 @@
+package com.mar.shuatigou.esdao;
+
+import com.mar.shuatigou.model.dto.post.PostEsDTO;
+import java.util.List;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 帖子 ES 操作
+ *
+ */
+public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
+
+    List<PostEsDTO> findByUserId(Long userId);
+}
---
+++ shuatigou-next-backend/src/main/java/com/mar/shuatigou/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.mar.shuatigou.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mar.shuatigou.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
