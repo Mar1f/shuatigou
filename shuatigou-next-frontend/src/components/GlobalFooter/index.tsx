20978adbfeddbfbb46c5ba180cc35c3b692d66b0
Mar1f <938224402@qq.com>
2024-11-07 11:31:29 +0800

    Add Ant Design layout and global footer

---
+++ shuatigou-next-frontend/src/components/GlobalFooter/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './index.css';
+/**
+ * 全局底部栏组件
+ * @constructor
+ */
+export default function GlobalFooter() {
+    const currentYear = new Date().getFullYear();
+    return (
+        <div className="global-footer">
+            <div>© {currentYear} 刷题平台</div>
+            <div>
+                <a href="#" target="_blank">
+                    Mar1f
+                </a>
+            </div>
+        </div>
+    );
+}
