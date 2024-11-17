65f59fb7e458e6d7a575fa024e2e730ab83c6345
Mar1f <938224402@qq.com>
2024-11-17 22:47:41 +0800

    Add incremental sync job for Question data to Elasticsearch

--- shuatigou-next-backend/src/main/java/com/mar/shuatigou/mapper/QuestionMapper.java
+++ shuatigou-next-backend/src/main/java/com/mar/shuatigou/mapper/QuestionMapper.java
@@ -2,6 +2,10 @@ package com.mar.shuatigou.mapper;
 
 import com.mar.shuatigou.model.entity.Question;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Date;
+import java.util.List;
 
 /**
 * @author mar1
@@ -10,7 +14,11 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @Entity com.mar.shuatigou.model.entity.Question
 */
 public interface QuestionMapper extends BaseMapper<Question> {
-
+    /**
+     * 查询题目列表（包括已被删除的数据）
+     */
+    @Select("select * from question where updateTime >= #{minUpdateTime}")
+    List<Question> listQuestionWithDelete(Date minUpdateTime);
 }
 
 
